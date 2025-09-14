59b36a9fdbaa3a6d8c6540ded676532248cf0a3e
Firamawaty Hia <fmawatyhia@gmail.com>
2025-09-14 22:46:52 +0700

    Add files via upload

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+// Rename this file to config.php and adjust your DB credentials.
+return [
+  'db_host' => 'localhost',
+  'db_name' => 'db_koperasi',
+  'db_user' => 'root',
+  'db_pass' => '',
+  'app_name' => 'KOPERASI',
+];
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+  'db_host' => 'localhost',
+  'db_name' => 'db_koperasi',
+  'db_user' => 'root',
+  'db_pass' => '',
+  'app_name' => 'KOPERASI',
+];
